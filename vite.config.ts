ec0c528be369471feb5375aca15c08c1592542ac
Seb Rauschert <sebastian.rauschert@insigene.com>
2025-01-31 12:08:13 +0800

    Fix base URL and deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/sebrauschert.github.io/',
   css: {
     postcss: {
       plugins: [
