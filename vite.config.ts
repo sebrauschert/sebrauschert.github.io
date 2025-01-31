e1ef821523794ddc39e6d22da1f7337e0ca43268
Seb Rauschert <sebastian.rauschert@insigene.com>
2025-01-31 12:12:22 +0800

    Update configuration for root-level GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sebrauschert.github.io/',
+  base: '/',
   css: {
     postcss: {
       plugins: [
